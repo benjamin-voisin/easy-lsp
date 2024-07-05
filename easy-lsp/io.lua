0f74856ab477334b7487da206b648b19896534bf
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-07-05 12:59:29 +0100

    👽️ update logging to use log module

--- easy-lsp/io.lua
+++ easy-lsp/io.lua
@@ -4,11 +4,17 @@ local rpc = require 'easy-lsp.rpc'
 
 function M.GetMessage()
 	local first_line = io.read()
+	Log.info("Got a new message: ", first_line)
 	local length = rpc.get_content_length(first_line)
+	Log.info("Length of the message: ", length)
 	local rest_message = io.read(length + 2)
-	io.write(rest_message)
+	Log.info("Content of the message: ", rest_message)
+	return rest_message
+end
+
+function M.SendMessage(message)
+	io.write(message)
 	io.flush()
-	return first_line..rest_message
 end
 
 
