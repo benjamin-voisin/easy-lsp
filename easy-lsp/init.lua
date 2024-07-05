58bb58f4863bbd8c6c47c097c70b1cdcf41d2c48
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-07-05 13:00:41 +0100

    🚧 responds to the "initialize" request !

--- easy-lsp/init.lua
+++ easy-lsp/init.lua
@@ -1,17 +1,41 @@
 local M = {}
+
 local rpc = require('easy-lsp.rpc')
+local lsp_io = require('easy-lsp.io')
+Log = require('easy-lsp.log')
 
-
-
+Log.outfile = "/tmp/easy-lsp.log"
 
 function M.start ()
-	local log = io.open("./easy-lsp.log", "w")
-	io.output(log)
-	local first_line = io.read()
-	local length = rpc.get_content_length(first_line)
-	local rest_message = io.read(length + 2)
-	io.write(rest_message)
-	io.flush()
+	while true do
+		local request = lsp_io.GetMessage()
+		if not request then
+			error("Problème we did not receive a message")
+		end
+		local err
+		request, err = rpc.DecodeJson(request)
+		if err then
+			error("Problème: Decoding didn't work properly: "..err)
+		end
+		Log.info("Méthode: ", request.method)
+		if request.method == "initialize" then
+			local response = {
+				id = request.id,
+				result = {
+					capabilities = {},
+					serverInfo = {
+						name = "Squirrel-Prover-LSP",
+						version = "0.1.0",
+					}
+				}
+			}
+			local response_message = rpc.EncodeMessage(response)
+			Log.info("Response: ", response_message)
+			lsp_io.SendMessage(response_message)
+		else
+			Log.warn("Cannot treat this message")
+		end
+	end
 end
 
 return M
