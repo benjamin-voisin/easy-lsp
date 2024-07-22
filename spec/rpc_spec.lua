176ff595ebed82233fa236206f741757362f7d26
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-07-22 17:04:17 +0100

    ✅ reorder elements in `asserts` to fit busted

--- spec/rpc_spec.lua
+++ spec/rpc_spec.lua
@@ -5,12 +5,12 @@ local rpc = require 'easy-lsp.rpc'
 describe("Decoding functions", function ()
 	local message = "Content-Length: 15\r\n\r\n{\"Method\":true}"
 	it("Should find the correct Content-Length", function ()
-		assert.are.equal(rpc.get_content_length(message), 15)
+		assert.are.equal(15, rpc.get_content_length(message))
 	end)
 
 	it("Decode corectly the message", function()
 		local result, err = rpc.DecodeMessage(message)
-		assert.are.same(rpc.DecodeMessage(message), {Method = true})
+		assert.are.same({Method = true}, rpc.DecodeMessage(message))
 	end)
 end)
 
@@ -18,6 +18,6 @@ describe("Encoding functions", function ()
 	it("Should make a good message with header and content", function ()
 		local message = {testing = true}
 		local expected_result = "Content-Length: 16\r\n\r\n{\"testing\":true}"
-		assert.are.equal(rpc.EncodeMessage(message), expected_result)
+		assert.are.equal(expected_result, rpc.EncodeMessage(message))
 	end)
 end)
